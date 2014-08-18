b55b68c455ab588293d52629c8f6aa1c03d2ece0
Robert Ernst <rfernst@gmail.com>
2014-08-18 14:12:57 +0200

    Added sort function to bedConverter

--- bedConverter.pl
+++ bedConverter.pl
@@ -12,28 +12,32 @@ my $bed_file;
 my $genome_dict_file;
 my $gatk = '';
 my $picard = '';
+my $sort = '';
 
 die usage() if @ARGV == 0;
 GetOptions (
 	'bed=s' => \$bed_file,
 	'dict=s' => \$genome_dict_file,
 	'gatk' => \$gatk,
-	'picard' => \$picard)
+	'picard' => \$picard,
+	'sort' => \$sort)
 or die usage();
 
 die usage() unless $bed_file;
 die usage() unless $genome_dict_file;
 
 ### Functionality
-### remove "chr" from chromosomes ???
-### sort ???
+if ($sort) {
+	my $input_bed_file = $bed_file;
+	$bed_file =~ s/.bed/_sorted.bed/;
+	`awk '{FS=""; print "chr"\$0}' $input_bed_file | sed 's/chrX/chr23/g' | sed 's/chrY/chr24/g'|sed 's/chrMT/chr25/g' | sort -k 1.4,1n -k 2,2n -k 3,3n | sed 's/chr23/chrX/g' |  sed 's/chr24/chrY/g' | sed 's/chr25/chrMT/g' | sed 's/^chr//g' > $bed_file`
+}
 
 ### gatk list file
 if ($gatk) {
 	my $gatk_file = $bed_file;
 	$gatk_file =~ s/.bed/_gatk.list/;
 
-	print "\n $gatk_file \n";
 	`awk {'print \$1":"\$2"-"\$3'} $bed_file > $gatk_file`
 }
 
@@ -42,7 +46,6 @@ if ($picard){
 	my $picard_file = $bed_file;
 	$picard_file =~ s/.bed/_picard.bed/;
 
-	print "\n $picard_file \n";
 	`cat $genome_dict_file > $picard_file`;
 	`awk -v OFS='\t' {'print \$1,\$2,\$3,\$4,\$5'} $bed_file >> $picard_file`;
 }
