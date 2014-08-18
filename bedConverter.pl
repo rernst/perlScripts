bc5674004c86d9b596744ab124617192d29b6f29
Robert Ernst <rfernst@gmail.com>
2014-08-18 11:19:54 +0200

    added gatk and picard support

--- bedConverter.pl
+++ bedConverter.pl
@@ -10,26 +10,47 @@ use Getopt::Long;
 ### Parse and check input arguments
 my $bed_file;
 my $genome_dict_file;
+my $gatk = '';
+my $picard = '';
 
 die usage() if @ARGV == 0;
 GetOptions (
 	'bed=s' => \$bed_file,
 	'dict=s' => \$genome_dict_file,
-) or die usage();
+	'gatk' => \$gatk,
+	'picard' => \$picard)
+or die usage();
 
 die usage() unless $bed_file;
 die usage() unless $genome_dict_file;
 
 ### Functionality
-### remove "chr" from chromosomes
-### sort bed file
+### remove "chr" from chromosomes ???
+### sort ???
+
 ### gatk list file
+if ($gatk) {
+	my $gatk_file = $bed_file;
+	$gatk_file =~ s/.bed/_gatk.list/;
+
+	print "\n $gatk_file \n";
+	`awk {'print \$1":"\$2"-"\$3'} $bed_file > $gatk_file`
+}
+
 ### picard bed file (header + max 5 columns)
+if ($picard){
+	my $picard_file = $bed_file;
+	$picard_file =~ s/.bed/_picard.bed/;
+
+	print "\n $picard_file \n";
+	`cat $genome_dict_file > $picard_file`;
+	`awk -v OFS='\t' {'print \$1,\$2,\$3,\$4,\$5'} $bed_file >> $picard_file`;
+}
 
 ### Functions
 sub usage{
 	warn <<END;
-	Usage: perl bedConverter.pl -bed [file.bed] -dict [genome.dict] 
+	Usage: perl bedConverter.pl -bed <file.bed> -dict <genome.dict> [-gatk -picard]
 END
 	exit;
 }
